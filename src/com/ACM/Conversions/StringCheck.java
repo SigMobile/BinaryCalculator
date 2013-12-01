96aacf8b83773b39e0ad4fba276bae318e5418c2
Kevin <kaveman1105@gmail.com>
2013-11-30 22:05:36 -0500

    working on getting special cases to work correctly

--- src/com/ACM/Conversions/StringCheck.java
+++ src/com/ACM/Conversions/StringCheck.java
@@ -8,20 +8,33 @@ public class StringCheck {
 		int i = 0;
 
 		while (i < string.length()) {
-			if (string.charAt(i) == 'x')
+			if (string.charAt(i) == 'x') {
 				this.correctedExpression += "*";
-			else
-				this.correctedExpression += string.charAt(i);
-			if (string.charAt(i) == ')')
-				if (string.charAt(i + 1) != '+' || string.charAt(i + 1) != '-'
-						|| string.charAt(i + 1) != 'x'
-						|| string.charAt(i + 1) != '/'
-						|| string.charAt(i + 1) != ')')
+				i++;
+				continue;
+			}
+			/*
+			 * if (string.charAt(i) == '(') { this.correctedExpression += "( ";
+			 * i++; continue; }
+			 */
+			if (string.charAt(i) == ')') {
+				this.correctedExpression += ")";
+				if ((i + 1) == string.length()) //end of string so break
+					break;
+
+				if (string.charAt(i + 1) >= '0' && string.charAt(i + 1) <= '9') {
 					this.correctedExpression += " x ";
+					i++;
+					continue;
+				}
+			}
+			this.correctedExpression += string.charAt(i);
 			i++;
+
 		}
 	}
-	public String getCorrectedExpression (){
+
+	public String getCorrectedExpression() {
 		return this.correctedExpression;
 	}
 }
