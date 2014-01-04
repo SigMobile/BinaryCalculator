75c490f3bececefc9aaed1d0d1988b860b4bc4ae
JimVanG <jjvg@me.com>
2014-01-04 11:49:11 -0500

    Added commens and tightened logic

--- src/com/ACM/binarycalculator/ExpressionHouse.java
+++ src/com/ACM/binarycalculator/ExpressionHouse.java
@@ -2,6 +2,13 @@ package com.ACM.binarycalculator;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author James Van Gaasbeck, ACM at UCF <jjvg@knights.ucf.edu>
+ * 
+ *         This is our data model class. It is used to hold all the expressions.
+ * 
+ */
 public class ExpressionHouse extends ArrayList<String> {
 
 	/**
@@ -10,14 +17,24 @@ public class ExpressionHouse extends ArrayList<String> {
 	private static final long serialVersionUID = 1882699854378946219L;
 	ArrayList<String> listOfAllExpressions;
 
+	/**
+	 * Constructor
+	 */
 	public ExpressionHouse() {
 		listOfAllExpressions = new ArrayList<String>();
 	}
 
+	/**
+	 * 
+	 * @param expressionToBeAdded
+	 *            - The expression/answer that is to be added to the list. This
+	 *            method is also called when the backspace button was hit.
+	 */
 	public void updateExpressions(String expressionToBeAdded) {
 
 		// if the expression contains a new line then that means it was an
 		// answer so we need to insert it into a new index in the arrayList
+		// and then create a new index after the answer.
 		if (expressionToBeAdded.contains("\n")) {
 			this.listOfAllExpressions.add(expressionToBeAdded);
 			// add a blank index to the end of the list to be overwritten when
@@ -38,24 +55,34 @@ public class ExpressionHouse extends ArrayList<String> {
 		}
 	}
 
+	/**
+	 * Method to clear all the expressions.
+	 */
 	public void clearAllExpressions() {
 		// create a new blank arrayList if the user hits clearAll
 		this.listOfAllExpressions = new ArrayList<String>();
 	}
 
+	/**
+	 * 
+	 * @return - Returns the current/last expression added. If the last
+	 *         expression was an answer, it will return a new blank
+	 *         expression/index.
+	 */
 	public String getCurrentExpression() {
-		return this.listOfAllExpressions
-				.get(this.listOfAllExpressions.size() - 1);
+		// if size is zero then just return a blank expression.
+		if (this.listOfAllExpressions.size() == 0)
+			return "";
+		else
+			return this.listOfAllExpressions.get(this.listOfAllExpressions
+					.size() - 1);
 	}
 
-	// public void removeLastCharacter(String expressionToBeUpdated) {
-	// // instead of actually removing the expression from the list lets just
-	// // update the last index with the currentExpression after the last char
-	// // was deleted in the front end
-	// this.listOfAllExpressions.set(this.listOfAllExpressions.size() - 1,
-	// expressionToBeUpdated);
-	// }
-
+	/**
+	 * 
+	 * @return - Returns a string of all the elements in the list. This method
+	 *         is used for filling the TextViews.
+	 */
 	public String printAllExpressions() {
 		StringBuilder retVal = new StringBuilder();
 
